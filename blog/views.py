11fa48b4ee7f3c2c628c302332d486bb1f350103
duke-doki <145125297+duke-doki@users.noreply.github.com>
2023-12-01 20:23:41 +0300

    remove redundant functions

--- blog/views.py
+++ blog/views.py
@@ -3,20 +3,6 @@ from django.shortcuts import render
 from blog.models import Comment, Post, Tag
 
 
-def serialize_post(post):
-    return {
-        'title': post.title,
-        'teaser_text': post.text[:200],
-        'author': post.author.username,
-        'comments_amount': len(Comment.objects.filter(post=post)),
-        'image_url': post.image.url if post.image else None,
-        'published_at': post.published_at,
-        'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.all()],
-        'first_tag_title': post.tags.all()[0].title,
-    }
-
-
 def serialize_post_optimized(post):
     return {
         'title': post.title,
@@ -28,13 +14,6 @@ def serialize_post_optimized(post):
         'slug': post.slug,
         'tags': [serialize_tag_optimized(tag) for tag in post.tags.all()],
         'first_tag_title': post.tags.all()[0].title,
-    }
-
-
-def serialize_tag(tag):
-    return {
-        'title': tag.title,
-        'posts_with_tag': len(Post.objects.filter(tags=tag)),
     }
 
 
